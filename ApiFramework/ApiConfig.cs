9eb2ce27ca77f97258e15d95206fc4049be977cb
zhangcs <zhangcs@Ctrip.com>
2016-02-19 16:52:31 +0800

    modify frameowrk

--- ApiFramework/ApiConfig.cs
+++ ApiFramework/ApiConfig.cs
@@ -3,17 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using ApiFramework.Extensions;
 
 namespace ApiFramework
 {
     public class ApiConfig
     {
         protected SelfHttpListenerBase HttpListener { get; private set; }
-        protected Dictionary<string, RestHandler> Handlers { get; private set; }
+        protected Dictionary<string, RequestPath> RequestPathCollection { get; private set; }
 
-        protected void RegisterHanders()
+        public ApiConfig()
         {
+            RequestPathCollection = new Dictionary<string, RequestPath>();
+        }
+        protected void RegisterRequestPath(params Assembly[] assembliesWithServices)
+        {
+            if (assembliesWithServices != null)
+            {
+                List<Type> types = GetAssemblyTypes(assembliesWithServices);
+                List<Type> serviceTypes = new List<Type>();
+                foreach (var type in types)
+                {
+                    if (type.HasAttribute<RestControllerAttribute>())
+                    {
+                        AnalysisRequestPath(type);
+                    }
+                }
+            }
+        }
 
+        private void AnalysisRequestPath(Type serviceType)
+        {
+            if (serviceType == null) return;
+            string basePath = "";
+            RequestPathAttribute reqAttribute = serviceType.GetCustomAttribute<RequestPathAttribute>();
+            if (reqAttribute != null)
+            {
+                if (!string.IsNullOrWhiteSpace(reqAttribute.Path))
+                {
+                    basePath += reqAttribute.Path.Trim();
+                }
+            }
+
+        }
+
+        private List<Type> GetAssemblyTypes(Assembly[] assembliesWithServices)
+        {
+            var results = new List<Type>();
+
+            try
+            {
+                foreach (var assembly in assembliesWithServices)
+                {
+                    foreach (var type in assembly.GetTypes())
+                    {
+                        results.Add(type);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return results;
         }
     }
 }
---
+++ ApiFramework/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiFramework.Extensions
+{
+    public static class ReflectionExtensions
+    {
+        public static bool HasAttribute<T>(this Type type, bool inherit = true) where T : Attribute
+        {
+            return type.GetCustomAttributes(inherit).Any(o => o.GetType() == typeof(T));
+        }
+    }
+}
