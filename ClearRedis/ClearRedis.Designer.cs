38bc806a0c46eeb0dd8cc6e7b138908d71fef029
zhangcs <zhangcs@Ctrip.com>
2016-05-03 11:22:35 +0800

    add redis

---
+++ ClearRedis/ClearRedis.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ClearRedis
+{
+    partial class ClearRedis
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.lbKey = new System.Windows.Forms.Label();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(79, 17);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(219, 21);
+            this.txtKey.TabIndex = 0;
+            // 
+            // lbKey
+            // 
+            this.lbKey.AutoSize = true;
+            this.lbKey.Location = new System.Drawing.Point(32, 20);
+            this.lbKey.Name = "lbKey";
+            this.lbKey.Size = new System.Drawing.Size(41, 12);
+            this.lbKey.TabIndex = 1;
+            this.lbKey.Text = "CardNo";
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(336, 14);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnQuery.TabIndex = 2;
+            this.btnQuery.Text = "Inquire";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(437, 14);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(79, 82);
+            this.txtValue.Multiline = true;
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(433, 207);
+            this.txtValue.TabIndex = 4;
+            this.txtValue.TextChanged += new System.EventHandler(this.txtValue_TextChanged);
+            // 
+            // ClearRedis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 323);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnQuery);
+            this.Controls.Add(this.lbKey);
+            this.Controls.Add(this.txtKey);
+            this.MaximizeBox = false;
+            this.Name = "ClearRedis";
+            this.Text = "ClearRedis";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Label lbKey;
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtValue;
+    }
+}
+
---
+++ ClearRedis/Common/CRedisUtility.cs
@@ -0,0 +1,50 @@
+﻿//==============================================================
+//Copyright (C) 2016 Ctrip Corporation.. All rights reserved.
+//Information Contained Herein is Proprietary and Confidential.
+//==============================================================
+//Create by zcs at 2016/4/29 18:14:41.
+//Description:
+//==============================================================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CRedis.Client;
+
+namespace ClearRedis.Common
+{
+    public class CRedisUtility
+    {
+        private static string _clusterName = "FncMayFly";
+
+        public static string GetValue(string key)
+        {
+            List<string> values = null;
+            if (!string.IsNullOrWhiteSpace(key))
+            {
+                ICacheProvider cache = CacheFactory.GetPorvider(_clusterName);
+                values = cache.GetAllItemsFromList(key);
+            }
+            if (values != null && values.Count > 0)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (string v in values)
+                {
+                    sb.AppendLine(v);
+                }
+                return sb.ToString();
+            }
+            return string.Empty;
+        }
+
+        public static void ClearValue(string key)
+        {
+            if (!string.IsNullOrWhiteSpace(key))
+            {
+                ICacheProvider cache = CacheFactory.GetPorvider(_clusterName);
+                cache.Remove(key);
+            }
+        }
+    }
+}
